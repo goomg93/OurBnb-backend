6df47ebc72c7c68d636fcc0f96291dde94900b24
guminki <rnalsrl93@gmail.com>
2022-05-26 22:55:43 +0900

    totalRows error 해결

--- models/RoomDao.js
+++ models/RoomDao.js
@@ -24,7 +24,8 @@ const getRoomList = async (
 	limit,
 	offset,
 ) => {
-	const roomList = await prisma.$queryRaw`
+	const [roomList, [{ totalRows }]] = await prisma.$transaction([
+		prisma.$queryRaw`
     SELECT SQL_CALC_FOUND_ROWS
       rooms.id AS roomId,
       rooms.name AS roomName,
@@ -83,42 +84,10 @@ const getRoomList = async (
     ORDER BY rooms.id
     LIMIT ${limit}
     OFFSET ${offset}
-  `;
-
-	const [{ totalRows }] = await prisma.$queryRaw`
-  SELECT
-  COUNT(*) AS totalRows
-  FROM
-  (SELECT
-    rooms.id
-  FROM rooms
-  JOIN hosts ON hosts.id = rooms.host_id
-  JOIN room_types ON room_types.id = rooms.room_type_id
-  JOIN locations ON locations.id = rooms.location_id
-  JOIN rooms_options ON rooms_options.room_id = rooms.id
-  JOIN options ON options.id = rooms_options.option_id
-  WHERE
-  -- location이 포함된 컬럼만 선택
-    locations.name LIKE CONCAT("%",${location},"%")
-  AND
-  -- 내가 선택한 체크인, 체크아웃 범위에 포함이 안된 컬럼만 선택
-    rooms.id NOT IN (SELECT reservations.room_id FROM reservations WHERE reservations.check_in BETWEEN '2022-01-19' AND '2022-01-26')
-  AND
-    rooms.id NOT IN (SELECT reservations.room_id FROM reservations WHERE reservations.check_out BETWEEN '2022-01-19' AND '2022-01-26')
-  AND
-  -- 사람수가 있다면 선택한 사람수보다 수용범위가 큰 컬럼만 선택
-    if(${person}, rooms.guest_capacity >= ${person} , rooms.guest_capacity is NOT NULL)
-  AND
-  -- 선택한 가격범위가 있다면 그 사이에 있는 컬럼만 선택
-    if(${false} , rooms.price BETWEEN 10000 AND 150000 , rooms.price is NOT NULL)
-  AND
-  -- 숙소유형을 선택했다면 숙소유형에 해당하는 컬럼만 선택
-    if(${roomTypeIdForSort} , room_types.id IN (${roomTypeIdForSort}) , room_types.id is NOT NULL)
-  AND
-  -- 선택한 편의시설의 id 값들이 있는 컬럼만 선택
-    if(${optionIdForSort}, options.id IN (${optionIdForSort}), options.id is NOT NULL)
-  GROUP BY rooms.id) AS cnt
-  `;
+  `,
+		prisma.$queryRaw`
+  SELECT FOUND_ROWS() AS totalRows`,
+	]);
 
 	return { totalRows, roomList };
 };
